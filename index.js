1e2357e44fe8de4539d96b40bfd1d9da445d37c2
Venu <venu6696@gmail.com>
2022-02-12 15:03:14 +0530

    prepare dir for heroku deployment

--- index.js
+++ index.js
@@ -2,6 +2,29 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
+let persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+];
+
 app.use(express.json());
 
 // app.use(morgan('tiny'));
@@ -28,29 +51,6 @@ app.use(
     ].join(' ');
   })
 );
-
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>');
@@ -125,7 +125,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
