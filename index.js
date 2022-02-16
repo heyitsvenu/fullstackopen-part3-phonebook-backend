cfbd9b37442c42120d9f4eba4a593b6d1bff53f1
Venu <venu6696@gmail.com>
2022-02-16 12:27:59 +0530

    connect mongo to main app

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const Person = require('./models/person');
 const morgan = require('morgan');
 const cors = require('cors');
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
+// let persons = [
+//   {
+//     id: 1,
+//     name: 'Arto Hellas',
+//     number: '040-123456',
+//   },
+//   {
+//     id: 2,
+//     name: 'Ada Lovelace',
+//     number: '39-44-5323523',
+//   },
+//   {
+//     id: 3,
+//     name: 'Dan Abramov',
+//     number: '12-43-234345',
+//   },
+//   {
+//     id: 4,
+//     name: 'Mary Poppendieck',
+//     number: '39-23-6423122',
+//   },
+// ];
 
 app.use(express.json());
 app.use(cors());
@@ -70,7 +72,9 @@ app.get('/info', (request, response) => {
 });
 
 app.get('/api/persons', (request, response) => {
-  response.status(200).json(persons);
+  Person.find({}).then((persons) => {
+    response.status(200).json(persons);
+  });
 });
 
 const generateId = (min, max) => {
@@ -84,28 +88,38 @@ const generateId = (min, max) => {
 };
 
 app.post('/api/persons', (request, response) => {
-  const id = generateId(1, 100000);
+  // const id = generateId(1, 100000);
 
   const body = request.body;
 
-  if (!body.name || !body.number) {
+  // if (!body.name || !body.number) {
+  //   return response.status(400).json({
+  //     error: 'missing field',
+  //   });
+  // } else if (persons.find((person) => person.name === body.name)) {
+  //   return response.status(400).json({
+  //     error: 'name must be unique',
+  //   });
+  // }
+
+  if (body.name === undefined || body.number === undefined) {
     return response.status(400).json({
       error: 'missing field',
     });
-  } else if (persons.find((person) => person.name === body.name)) {
-    return response.status(400).json({
-      error: 'name must be unique',
-    });
   }
 
-  const person = {
-    id: id,
+  const person = new Person({
+    // id: id,
     name: body.name,
     number: body.number,
-  };
+  });
 
-  persons = persons.concat(person);
-  response.status(201).json(person);
+  // persons = persons.concat(person);
+  // response.status(201).json(person);
+
+  person.save().then((savedPerson) => {
+    response.status(201).json(savedPerson);
+  });
 });
 
 app.get('/api/persons/:id', (request, response) => {
@@ -128,7 +142,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end();
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
